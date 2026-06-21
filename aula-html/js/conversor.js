6fab1ec72bec2b4de43b39834dc957f90578f6e3
Diego De Souza Rodrigues <diegodesouzarodriguesoficial@gmail.com>
2026-06-20 23:43:50 -0300

    Add files via upload

---
+++ aula-html/js/conversor.js
@@ -0,0 +1,15 @@
+
+//função
+
+
+function Converter (){ 
+
+const ValorReal = document.querySelector ("input")
+
+const  valorConvertido = ValorReal.value / 5
+
+alert(`valorConvertido é ${valorConvertido}`)
+
+
+
+}
